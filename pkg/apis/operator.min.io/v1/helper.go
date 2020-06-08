minio/operator

commit badb55bb596606732694ac3e6df98d67e60ebecd

Add flag for go template to format host names (#130)
When using an alternate DNS like external-dns, your host names will be
different than the internal core-dns format.  This adds a flag so that
the user can specify the name format using a go template.

pkg/apis/operator.min.io/v1/helper.go
@@ -18,6 +18,7 @@
 package v1
 
 import (
+	"bytes"
 	"context"
 	"crypto/tls"
 	"errors"
@@ -26,6 +27,7 @@ import (
 	"net/http"
 	"path"
 	"strconv"
+	"text/template"
 	"time"
 
 	appsv1 "k8s.io/api/apps/v1"
@@ -38,6 +40,19 @@ import (
 	iampolicy "github.com/minio/minio/pkg/iam/policy"
 	"github.com/minio/minio/pkg/madmin"
 )
+
+type hostsTemplateValues struct {
+	StatefulSet string
+	CIService   string
+	HLService   string
+	Ellipsis    string
+	Domain      string
+}
+
+// ellipsis returns the host range string
+func ellipsis(start, end int) string {
+	return "{" + strconv.Itoa(start) + "..." + strconv.Itoa(end) + "}"
+}
 
 // HasCredsSecret returns true if the user has provided a secret
 // for a MinIOInstance else false
@@ -77,7 +92,7 @@ func (mi *MinIOInstance) VolumePath() string {
 	if mi.Spec.VolumesPerServer == 1 {
 		return path.Join(mi.Spec.Mountpath, mi.Spec.Subpath)
 	}
-	return path.Join(mi.Spec.Mountpath+"{0..."+strconv.Itoa((mi.Spec.VolumesPerServer)-1)+"}", mi.Spec.Subpath)
+	return path.Join(mi.Spec.Mountpath+ellipsis(0, mi.Spec.VolumesPerServer-1), mi.Spec.Subpath)
 }
 
 // MinIOReplicas returns the number of total replicas
@@ -203,7 +218,37 @@ func (mi *MinIOInstance) MinIOHosts() []string {
 	// Create the ellipses style URL
 	for _, z := range mi.Spec.Zones {
 		max = max + z.Servers
-		hosts = append(hosts, fmt.Sprintf("%s-{"+strconv.Itoa(int(index))+"..."+strconv.Itoa(int(max)-1)+"}.%s.%s.svc.%s", mi.MinIOStatefulSetName(), mi.MinIOHLServiceName(), mi.Namespace, ClusterDomain))
+		hosts = append(hosts, fmt.Sprintf("%s-%s.%s.%s.svc.%s", ellipsis(int(index), int(max)-1), mi.MinIOStatefulSetName(), mi.MinIOHLServiceName(), mi.Namespace, ClusterDomain))
+		index = max
+	}
+	return hosts
+}
+
+// TemplatedMinIOHosts returns the domain names in ellipses format created for current MinIOInstance without the service part
+func (mi *MinIOInstance) TemplatedMinIOHosts(hostsTemplate string) []string {
+	hosts := make([]string, 0)
+	tmpl, err := template.New("hosts").Parse(hostsTemplate)
+	if err != nil {
+		msg := "Invalid go template for hosts"
+		klog.V(2).Infof(msg)
+		return hosts
+	}
+	var max, index int32
+	// Create the ellipses style URL
+	for _, z := range mi.Spec.Zones {
+		max = max + z.Servers
+		data := hostsTemplateValues{
+			StatefulSet: mi.MinIOStatefulSetName(),
+			CIService:   mi.MinIOCIServiceName(),
+			HLService:   mi.MinIOHLServiceName(),
+			Ellipsis:    ellipsis(int(index), int(max)-1),
+			Domain:      ClusterDomain,
+		}
+		output := new(bytes.Buffer)
+		if err = tmpl.Execute(output, data); err != nil {
+			continue
+		}
+		hosts = append(hosts, output.String())
 		index = max
 	}
 	return hosts

main.go
@@ -44,9 +44,10 @@ import (
 var Version = "DEVELOPMENT.GOGET"
 
 var (
-	masterURL    string
-	kubeconfig   string
-	checkVersion bool
+	masterURL     string
+	kubeconfig    string
+	hostsTemplate string
+	checkVersion  bool
 
 	onlyOneSignalHandler = make(chan struct{})
 	shutdownSignals      = []os.Signal{os.Interrupt, syscall.SIGTERM}
@@ -55,6 +56,7 @@ var (
 func init() {
 	flag.StringVar(&kubeconfig, "kubeconfig", "", "path to a kubeconfig. Only required if out-of-cluster")
 	flag.StringVar(&masterURL, "master", "", "the address of the Kubernetes API server. Overrides any value in kubeconfig. Only required if out-of-cluster")
+	flag.StringVar(&hostsTemplate, "hosts-template", "", "the go template to use for hostname formatting of name fields (StatefulSet, CIService, HLService, Ellipsis, Domain)")
 	flag.BoolVar(&checkVersion, "version", false, "print version")
 }
 
@@ -113,7 +115,8 @@ func main() {
 		kubeInformerFactory.Apps().V1().Deployments(),
 		kubeInformerFactory.Batch().V1().Jobs(),
 		minioInformerFactory.Operator().V1().MinIOInstances(),
-		kubeInformerFactory.Core().V1().Services())
+		kubeInformerFactory.Core().V1().Services(),
+		hostsTemplate)
 
 	go kubeInformerFactory.Start(stopCh)
 	go minioInformerFactory.Start(stopCh)

pkg/apis/operator.min.io/v1/helper_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 func TestEnsureDefaults(t *testing.T) {
@@ -40,5 +41,41 @@ func TestEnsureDefaults(t *testing.T) {
 		mi.EnsureDefaults()
 
 		assert.Equal(t, newImage, mi.Spec.Image)
+	})
+}
+
+func TestTemplateVariables(t *testing.T) {
+	servers := 2
+	mi := MinIOInstance{
+		ObjectMeta: metav1.ObjectMeta{Name: "test"},
+		Spec: MinIOInstanceSpec{
+			Zones: []Zone{{"single", int32(servers)}},
+		},
+	}
+	mi.EnsureDefaults()
+
+	t.Run("StatefulSet", func(t *testing.T) {
+		hosts := mi.TemplatedMinIOHosts("{{.StatefulSet}}")
+		assert.Contains(t, hosts, mi.MinIOStatefulSetName())
+	})
+
+	t.Run("CIService", func(t *testing.T) {
+		hosts := mi.TemplatedMinIOHosts("{{.CIService}}")
+		assert.Contains(t, hosts, mi.MinIOCIServiceName())
+	})
+
+	t.Run("HLService", func(t *testing.T) {
+		hosts := mi.TemplatedMinIOHosts("{{.HLService}}")
+		assert.Contains(t, hosts, mi.MinIOHLServiceName())
+	})
+
+	t.Run("Ellipsis", func(t *testing.T) {
+		hosts := mi.TemplatedMinIOHosts("{{.Ellipsis}}")
+		assert.Contains(t, hosts, ellipsis(0, servers-1))
+	})
+
+	t.Run("Domain", func(t *testing.T) {
+		hosts := mi.TemplatedMinIOHosts("{{.Domain}}")
+		assert.Contains(t, hosts, ClusterDomain)
 	})
 }

pkg/controller/cluster/csr.go
@@ -67,7 +67,7 @@ func isEqual(a, b []string) bool {
 	return true
 }
 
-func generateCryptoData(mi *miniov1.MinIOInstance) ([]byte, []byte, error) {
+func generateCryptoData(mi *miniov1.MinIOInstance, hostsTemplate string) ([]byte, []byte, error) {
 	var dnsNames []string
 	klog.V(0).Infof("Generating private key")
 	privateKey, err := newPrivateKey(miniov1.DefaultEllipticCurve)
@@ -85,6 +85,9 @@ func generateCryptoData(mi *miniov1.MinIOInstance) ([]byte, []byte, error) {
 	klog.V(0).Infof("Generating CSR with CN=%s", mi.Spec.CertConfig.CommonName)
 
 	hosts := mi.AllMinIOHosts()
+	if hostsTemplate != "" {
+		hosts = mi.TemplatedMinIOHosts(hostsTemplate)
+	}
 
 	if isEqual(mi.Spec.CertConfig.DNSNames, hosts) {
 		dnsNames = mi.Spec.CertConfig.DNSNames
@@ -113,7 +116,7 @@ func generateCryptoData(mi *miniov1.MinIOInstance) ([]byte, []byte, error) {
 // finally creating a secret that MinIO statefulset will use to mount private key and certificate for TLS
 // This Method Blocks till the CSR Request is approved via kubectl approve
 func (c *Controller) createCSR(ctx context.Context, mi *miniov1.MinIOInstance) error {
-	privKeysBytes, csrBytes, err := generateCryptoData(mi)
+	privKeysBytes, csrBytes, err := generateCryptoData(mi, c.hostsTemplate)
 	if err != nil {
 		klog.Errorf("Private Key and CSR generation failed with error: %v", err)
 		return err

pkg/controller/cluster/kes-csr.go
@@ -101,7 +101,7 @@ func (c *Controller) createKESTLSCSR(ctx context.Context, mi *miniov1.MinIOInsta
 // createMinIOClientTLSCSR handles all the steps required to create the CSR: from creation of keys, submitting CSR and
 // finally creating a secret that KES Statefulset will use for MinIO Client Auth
 func (c *Controller) createMinIOClientTLSCSR(ctx context.Context, mi *miniov1.MinIOInstance) error {
-	privKeysBytes, csrBytes, err := generateCryptoData(mi)
+	privKeysBytes, csrBytes, err := generateCryptoData(mi, c.hostsTemplate)
 	if err != nil {
 		klog.Errorf("Private Key and CSR generation failed with error: %v", err)
 		return err

pkg/controller/cluster/main-controller.go
@@ -125,6 +125,9 @@ type Controller struct {
 	// recorder is an event recorder for recording Event resources to the
 	// Kubernetes API.
 	recorder record.EventRecorder
+
+	// Use a go template to render the hosts string
+	hostsTemplate string
 }
 
 // NewController returns a new sample controller
@@ -136,7 +139,8 @@ func NewController(
 	deploymentInformer appsinformers.DeploymentInformer,
 	jobInformer batchinformers.JobInformer,
 	minioInstanceInformer informers.MinIOInstanceInformer,
-	serviceInformer coreinformers.ServiceInformer) *Controller {
+	serviceInformer coreinformers.ServiceInformer,
+	hostsTemplate string) *Controller {
 
 	// Create event broadcaster
 	// Add minio-controller types to the default Kubernetes Scheme so Events can be
@@ -164,6 +168,7 @@ func NewController(
 		serviceListerSynced:     serviceInformer.Informer().HasSynced,
 		workqueue:               queue.NewNamedRateLimitingQueue(queue.DefaultControllerRateLimiter(), "MinIOInstances"),
 		recorder:                recorder,
+		hostsTemplate:           hostsTemplate,
 	}
 
 	klog.Info("Setting up event handlers")
@@ -411,7 +416,7 @@ func (c *Controller) syncHandler(key string) error {
 			if err != nil {
 				return err
 			}
-			ss = statefulsets.NewForMinIO(mi, hlSvc.Name)
+			ss = statefulsets.NewForMinIO(mi, hlSvc.Name, c.hostsTemplate)
 			ss, err = c.kubeClientSet.AppsV1().StatefulSets(mi.Namespace).Create(ctx, ss, cOpts)
 			if err != nil {
 				return err
@@ -450,7 +455,7 @@ func (c *Controller) syncHandler(key string) error {
 				}
 			}
 
-			ss = statefulsets.NewForMinIO(mi, hlSvc.Name)
+			ss = statefulsets.NewForMinIO(mi, hlSvc.Name, c.hostsTemplate)
 			klog.V(2).Infof("Removing the existing StatefulSet %s with replicas: %d", name, *ss.Spec.Replicas)
 			if err := c.kubeClientSet.AppsV1().StatefulSets(mi.Namespace).Delete(ctx, ss.Name, metav1.DeleteOptions{}); err != nil {
 				return err
@@ -476,7 +481,7 @@ func (c *Controller) syncHandler(key string) error {
 				return err
 			}
 			klog.V(4).Infof("Updating MinIOInstance %s MinIO server version %s, to: %s", name, mi.Spec.Image, ss.Spec.Template.Spec.Containers[0].Image)
-			ss = statefulsets.NewForMinIO(mi, hlSvc.Name)
+			ss = statefulsets.NewForMinIO(mi, hlSvc.Name, c.hostsTemplate)
 			if _, err := c.kubeClientSet.AppsV1().StatefulSets(mi.Namespace).Update(ctx, ss, uOpts); err != nil {
 				return err
 			}

pkg/resources/statefulsets/minio-statefulset.go
@@ -183,7 +183,7 @@ func probes(mi *miniov1.MinIOInstance) (readiness, liveness *corev1.Probe) {
 }
 
 // Builds the MinIO container for a MinIOInstance.
-func minioServerContainer(mi *miniov1.MinIOInstance, serviceName string) corev1.Container {
+func minioServerContainer(mi *miniov1.MinIOInstance, serviceName string, hostsTemplate string) corev1.Container {
 	args := []string{"server", "--certs-dir", "/tmp/certs"}
 
 	if mi.Spec.Zones[0].Servers == 1 {
@@ -191,7 +191,11 @@ func minioServerContainer(mi *miniov1.MinIOInstance, serviceName string) corev1.
 		args = append(args, mi.VolumePath())
 	} else {
 		// append all the MinIOInstance replica URLs
-		for _, h := range mi.MinIOHosts() {
+		hosts := mi.MinIOHosts()
+		if hostsTemplate != "" {
+			hosts = mi.TemplatedMinIOHosts(hostsTemplate)
+		}
+		for _, h := range hosts {
 			args = append(args, fmt.Sprintf("%s://"+h+"%s", miniov1.Scheme, mi.VolumePath()))
 		}
 	}
@@ -245,7 +249,7 @@ func getVolumesForContainer(mi *miniov1.MinIOInstance) []corev1.Volume {
 }
 
 // NewForMinIO creates a new StatefulSet for the given Cluster.
-func NewForMinIO(mi *miniov1.MinIOInstance, serviceName string) *appsv1.StatefulSet {
+func NewForMinIO(mi *miniov1.MinIOInstance, serviceName string, hostsTemplate string) *appsv1.StatefulSet {
 	// If a PV isn't specified just use a EmptyDir volume
 	var podVolumes = getVolumesForContainer(mi)
 	var replicas = mi.MinIOReplicas()
@@ -327,7 +331,7 @@ func NewForMinIO(mi *miniov1.MinIOInstance, serviceName string) *appsv1.Stateful
 		})
 	}
 
-	containers := []corev1.Container{minioServerContainer(mi, serviceName)}
+	containers := []corev1.Container{minioServerContainer(mi, serviceName, hostsTemplate)}
 
 	ss := &appsv1.StatefulSet{
 		ObjectMeta: metav1.ObjectMeta{

commit e0ae8d274922409e881b05377243e85100fbda05

Add a way to apply PodSecurityPolicy to MinIO Pods
This PR also fixes a regression introduced in #130 where incorrect
arguements are passed to MinIO containers.

examples/minioinstance-pod-security-policy.yaml
@@ -1,0 +1,187 @@
+apiVersion: v1
+kind: Secret
+metadata:
+  name: minio-creds-secret
+type: Opaque
+data:
+  accesskey: bWluaW8= # base 64 encoded "minio" (echo -n 'minio' | base64)
+  secretkey: bWluaW8xMjM= # based 64 encoded "minio123" (echo -n 'minio123' | base64)
+---
+apiVersion: v1
+kind: Service
+metadata:
+  name: minio-service
+spec:
+  type: ClusterIP
+  ports:
+    - port: 9000
+      targetPort: 9000
+      protocol: TCP
+      # Optional field
+      # By default and for convenience, the Kubernetes control plane will allocate a port from a range (default: 30000-32767)
+      # nodePort: 30007
+  selector:
+    app: minio
+---
+apiVersion: v1
+kind: ServiceAccount
+metadata:
+  name: minio-pods
+  namespace: default
+---
+apiVersion: policy/v1beta1
+kind: PodSecurityPolicy
+metadata:
+  name: minio-pods-policy
+spec:
+  privileged: false
+  allowPrivilegeEscalation: false
+  hostNetwork: true
+  seLinux:
+    rule: RunAsAny
+  supplementalGroups:
+    rule: RunAsAny
+  runAsUser:
+    rule: RunAsAny
+  fsGroup:
+    rule: RunAsAny
+  volumes:
+  - '*'
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRole
+metadata:
+  name: minio-pods-role
+rules:
+- apiGroups: 
+  - "policy"
+  resources: 
+  - "podsecuritypolicies"
+  verbs: 
+  - "use"
+  resourceNames:
+  - "minio-pods-policy"
+---
+kind: ClusterRoleBinding
+apiVersion: rbac.authorization.k8s.io/v1beta1
+metadata:
+  name: minio-pods-binding
+roleRef:
+  apiGroup: rbac.authorization.k8s.io
+  kind: ClusterRole
+  name: minio-pods-role
+subjects:
+- kind: ServiceAccount
+  name: minio-pods
+  namespace: default
+---
+apiVersion: operator.min.io/v1
+kind: MinIOInstance
+metadata:
+  name: minio
+## If specified, MinIOInstance pods will be dispatched by specified scheduler.
+## If not specified, the pod will be dispatched by default scheduler.
+# scheduler:
+#  name: my-custom-scheduler
+spec:
+  ## Add metadata to the all pods created by the StatefulSet
+  metadata:
+    ## Optionally pass labels to be applied to the statefulset pods
+    labels:
+      app: minio
+    annotations:
+      prometheus.io/path: /minio/prometheus/metrics
+      prometheus.io/port: "9000"
+      prometheus.io/scrape: "true"
+  ## Registry location and Tag to download MinIO Server image
+  image: minio/minio:RELEASE.2020-06-03T22-13-49Z
+  ## A ClusterIP Service will be created with the given name
+  serviceName: minio-internal-service
+  ## Service account to be used for all the MinIO Pods
+  serviceAccountName: minio-pods
+  zones:
+    - name: "zone-0"
+      ## Number of MinIO servers/pods in this zone.
+      ## For standalone mode, supply 1. For distributed mode, supply 4 or more.
+      ## Note that the operator does not support upgrading from standalone to distributed mode.
+      servers: 4
+  ## Supply number of volumes to be mounted per MinIO server instance.
+  volumesPerServer: 4
+  ## Mount path where PV will be mounted inside container(s). Defaults to "/export".
+  mountPath: /export
+  ## Sub path inside Mount path where MinIO starts. Defaults to "".
+  # subPath: /data
+  ## This VolumeClaimTemplate is used across all the volumes provisioned for MinIO cluster.
+  ## Please do not change the volumeClaimTemplate field while expanding the cluster, this may
+  ## lead to unbound PVCs and missing data
+  volumeClaimTemplate:
+    metadata:
+      name: data
+    spec:
+      accessModes:
+        - ReadWriteOnce
+      resources:
+        requests:
+          storage: 1Ti
+  ## Secret with credentials to be used by MinIO instance.
+  credsSecret:
+    name: minio-creds-secret
+  ## PodManagement policy for pods created by StatefulSet. Can be "OrderedReady" or "Parallel"
+  ## Refer https://kubernetes.io/docs/tutorials/stateful-application/basic-stateful-set/#pod-management-policy
+  ## for details. Defaults to "Parallel"
+  ## If set to "OrderedReady", then disable Readiness checks below. Readiness check will only
+  ## work if PodManagementPolicy is set to "Parallel".
+  podManagementPolicy: Parallel
+  ## Secret with certificates to configure TLS for MinIO certs. Create secrets as explained
+  ## here: https://github.com/minio/minio/tree/master/docs/tls/kubernetes#2-create-kubernetes-secret
+  # externalCertSecret:
+  # name: tls-ssl-minio
+  ## Enable Kubernetes based certificate generation and signing as explained in
+  ## https://kubernetes.io/docs/tasks/tls/managing-tls-in-a-cluster
+  requestAutoCert: false
+  ## Used when "requestAutoCert" is set to true. Set CommonName for the auto-generated certificate.
+  ## Internal DNS name for the pod will be used if CommonName is not provided.
+  ## DNS name format is minio-{0...3}.minio.default.svc.cluster.local
+  certConfig:
+    commonName: ""
+    organizationName: []
+    dnsNames: []
+  ## Used to specify a toleration for a pod
+  # tolerations:
+  #  - effect: NoSchedule
+  #    key: dedicated
+  #    operator: Equal
+  #    value: storage
+  ## Add environment variables to be set in MinIO container (https://github.com/minio/minio/tree/master/docs/config)
+  # env:
+    # - name: MINIO_BROWSER
+    #  value: "off" # to turn-off browser
+    # - name: MINIO_STORAGE_CLASS_STANDARD
+    #   value: "EC:2"
+  ## Configure resource requests and limits for MinIO containers
+  # resources:
+    # requests:
+      # memory: 20Gi
+  ## Liveness probe detects situations where MinIO server instance
+  ## is not working properly and needs restart. Kubernetes automatically
+  ## restarts the pods if liveness checks fail.
+  liveness:
+    initialDelaySeconds: 120
+    periodSeconds: 60
+  ## Readiness probe detects situations when MinIO server instance
+  ## is not ready to accept traffic. Kubernetes doesn't forward
+  ## traffic to the pod while readiness checks fail.
+  ## Readiness check will only work if PodManagementPolicy is set to "Parallel".
+  ## Disable this check if you're setting PodManagementPolicy to "OrderedReady".
+  readiness:
+    initialDelaySeconds: 120
+    periodSeconds: 60
+  ## nodeSelector parameters for MinIO Pods. It specifies a map of key-value pairs. For the pod to be
+  ## eligible to run on a node, the node must have each of the
+  ## indicated key-value pairs as labels.
+  ## Read more here: https://kubernetes.io/docs/concepts/configuration/assign-pod-node/
+  # nodeSelector:
+  #   disktype: ssd
+  ## Affinity settings for MinIO pods. Read more about affinity
+  ## here: https://kubernetes.io/docs/concepts/configuration/assign-pod-node/#affinity-and-anti-affinity.
+  # affinity:

minio-operator.yaml
@@ -161,7 +161,7 @@ rules:
 apiVersion: v1
 kind: ServiceAccount
 metadata:
-  name: minio-operator-sa
+  name: minio-operator
   namespace: default
 ---
 kind: ClusterRoleBinding
@@ -174,7 +174,7 @@ roleRef:
   name: minio-operator-role
 subjects:
 - kind: ServiceAccount
-  name: minio-operator-sa
+  name: minio-operator
   namespace: default
 ---
 apiVersion: apps/v1
@@ -192,7 +192,7 @@ spec:
       labels:
         name: minio-operator
     spec:
-      serviceAccountName: minio-operator-sa
+      serviceAccountName: minio-operator
       containers:
         - name: minio-operator
           image: minio/k8s-operator:2.0.5

pkg/apis/operator.min.io/v1/helper.go
@@ -218,7 +218,7 @@ func (mi *MinIOInstance) MinIOHosts() []string {
 	// Create the ellipses style URL
 	for _, z := range mi.Spec.Zones {
 		max = max + z.Servers
-		hosts = append(hosts, fmt.Sprintf("%s-%s.%s.%s.svc.%s", ellipsis(int(index), int(max)-1), mi.MinIOStatefulSetName(), mi.MinIOHLServiceName(), mi.Namespace, ClusterDomain))
+		hosts = append(hosts, fmt.Sprintf("%s-%s.%s.%s.svc.%s", mi.MinIOStatefulSetName(), ellipsis(int(index), int(max)-1), mi.MinIOHLServiceName(), mi.Namespace, ClusterDomain))
 		index = max
 	}
 	return hosts