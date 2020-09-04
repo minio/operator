b8aa01dc5d70f84349c3591dc32212fb1d3a128d
Harshavardhana <harsha@minio.io>
2020-09-04 12:46:31 -0700

    add support for kubectl-minio windows/freebsd (#281)

--- minisign.sh
+++ minisign.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-minisign -s "/media/${USER}/minio/minisign.key" -Sm "${1}" < "/media/${USER}/minio/minisign-passphrase"
-cp -a "${1}".minisig "$(basename ${1})".minisig
+minisign -s "/media/${USER}/minio/minisign.key" \
+         -x "$(basename $(dirname ${1}))".minisig \
+         -Sm "${1}" < "/media/${USER}/minio/minisign-passphrase"
+
+if [ -f minio-operator_linux_amd64.minisig ]; then
+    mv minio-operator_linux_amd64.minisig minio-operator.minisig
+fi
