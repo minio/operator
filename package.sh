3b682e1332c7fb27bcc730501a6d5dc957ecc3a8
Harshavardhana <harsha@minio.io>
2020-09-15 10:50:48 -0700

    add tar.gz support for releases

--- package.sh
+++ package.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
+binary=$(basename "$(dirname "${1}")")
 minisign -s "/media/${USER}/minio/minisign.key" \
-         -x "$(basename $(dirname ${1}))".minisig \
+         -x "${binary}.minisig" \
          -Sm "${1}" < "/media/${USER}/minio/minisign-passphrase"
+
+cp -f "${binary}.minisig" "${1}.minisig"
+cp -f LICENSE "$(dirname "${1}")"
+tar -C "$(dirname "${1}")" --numeric-owner -czf "${binary}.tar.gz" kubectl*
 
 if [ -f minio-operator_linux_amd64.minisig ]; then
     mv minio-operator_linux_amd64.minisig minio-operator.minisig
