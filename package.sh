da8836c76579148e3eff5bb3042a56a275ca4497
Harshavardhana <harsha@minio.io>
2021-01-19 11:30:21 -0800

    add multi-arch build for minio-operator

--- package.sh
+++ package.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
 binary=$(basename "$(dirname "${1}")")
-minisign -s "/media/${USER}/minio/minisign.key" \
-         -x "${binary}.minisig" \
-         -Sm "${1}" < "/media/${USER}/minio/minisign-passphrase"
-
-cp -f "${binary}.minisig" "${1}.minisig"
-cp -f LICENSE "$(dirname "${1}")"
-zip -r -j "${binary}.zip" "$(dirname "${1}")"
-
-if [ -f minio-operator_linux_amd64.minisig ]; then
-    mv minio-operator_linux_amd64.minisig minio-operator.minisig
+if [[ "${binary}" =~ kubectl-minio* ]]; then
+    cp -f LICENSE "$(dirname "${1}")"
+    cp -f CREDITS "$(dirname "${1}")"
+    cp -f README.md  "$(dirname "${1}")"
+    zip -q -r -j "${binary}.zip" "$(dirname "${1}")"
 fi
