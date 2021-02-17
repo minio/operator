830b2dca738673156a559bdb7c9d8387522f062f
Daniel Valdivia <hola@danielvaldivia.com>
2021-02-17 13:38:55 -0800

    Fix NPE for adding SecurityContext on Log DB (#471)

--- pkg/resources/statefulsets/log.go
+++ pkg/resources/statefulsets/log.go
@@ -158,11 +158,12 @@ func NewForLogDb(t *miniov2.Tenant, serviceName string) *appsv1.StatefulSet {
 			Containers:         []corev1.Container{logDbContainer(t)},
 			RestartPolicy:      corev1.RestartPolicyAlways,
 			SchedulerName:      t.Scheduler.Name,
-			SecurityContext:    t.Spec.Log.Db.SecurityContext,
 		},
 	}
 	// if we have DB configurations to honor
 	if t.Spec.Log.Db != nil {
+		// Attach security Policy
+		dbPod.Spec.SecurityContext = t.Spec.Log.Db.SecurityContext
 		// attach affinity clauses
 		if t.Spec.Log.Db.Affinity != nil {
 			dbPod.Spec.Affinity = t.Spec.Log.Db.Affinity
