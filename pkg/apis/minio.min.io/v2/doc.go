6600c1e02b12c70473938009ad717256b4b0da57
ravindk89 <ravind@min.io>
2021-02-23 17:34:56 -0500

    Operator V4 CRD Docs Update
    Operator 4.0 CRD Reference Update
    
    Updated to master, fixed linting issues, deprecated spec.sidecars
    
    Update docs/templates/asciidoctor/gv_list.tpl
    
    Update docs/crd.adoc

--- pkg/apis/minio.min.io/v2/doc.go
+++ pkg/apis/minio.min.io/v2/doc.go
@@ -18,7 +18,13 @@
 // +k8s:deepcopy-gen=package,register
 // go:generate controller-gen crd:trivialVersions=true paths=. output:dir=.
 
-// Package v2 is the v2 version of the API.
+// Package v2 - This page provides a quick automatically generated reference for the MinIO Operator `minio.min.io/v2` CRD. For more complete documentation on the MinIO Operator CRD, see https://docs.min.io/minio/k8s/reference/minio-operator-reference[MinIO Kubernetes Documentation]. +
+//
+// The `minio.min.io/v2` API was released with the v4.0.0 MinIO Operator. The MinIO Operator automatically converts existing tenants using the `/v1` API to `/v2`. +
+//
+// The `minio.min.io/v1` API is deprecated and will be removed in a future release. Update your existing MinIO Tenant object specifications to the `/v2` API. For documentation on the `v1` API, see the https://github.com/minio/operator/blob/v4.0.0/docs/crd.adoc#k8s-api-minio-min-io-v1[MinIO Operator CRD v1 reference]. +
+//
+//
 // +groupName=minio.min.io
 // +versionName=v2
 package v2
