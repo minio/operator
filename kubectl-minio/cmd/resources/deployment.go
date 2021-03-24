4119ee1e4e0bbcf8e41ccf2c83871671c9d3fa61
Daniel Valdivia <hola@danielvaldivia.com>
2021-03-23 17:46:50 -0700

    Rework kubectl minio init and delete as kustomize based

--- kubectl-minio/cmd/resources/deployment.go
+++ kubectl-minio/cmd/resources/deployment.go
@@ -18,80 +18,22 @@
 
 package resources
 
-import (
-	"github.com/minio/kubectl-minio/cmd/helpers"
-	appsv1 "k8s.io/api/apps/v1"
-	corev1 "k8s.io/api/core/v1"
-	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
-)
-
 // OperatorOptions encapsulates the CLI options for a MinIO Operator
 type OperatorOptions struct {
-	Name            string
-	Image           string
-	Namespace       string
-	NSToWatch       string
-	ClusterDomain   string
-	ImagePullSecret string
-	ConsoleImage    string
+	Name               string
+	Image              string
+	Namespace          string
+	NSToWatch          string
+	ClusterDomain      string
+	ImagePullSecret    string
+	ConsoleImage       string
+	TenantMinIOImage   string
+	TenantConsoleImage string
+	TenantKesImage     string
 }
 
 func operatorLabels() map[string]string {
 	m := make(map[string]string)
 	m["name"] = "minio-operator"
 	return m
-}
-
-// Adds required Operator environment variables
-func envVars(clusterDomain, nsToWatch string) []corev1.EnvVar {
-	return []corev1.EnvVar{
-		{
-			Name:  "CLUSTER_DOMAIN",
-			Value: clusterDomain,
-		},
-		{
-			Name:  "WATCHED_NAMESPACE",
-			Value: nsToWatch,
-		},
-	}
-}
-
-// Builds the MinIO Operator container.
-func container(img, clusterDomain, nsToWatch string) corev1.Container {
-	return corev1.Container{
-		Name:            helpers.ContainerName,
-		Image:           img,
-		ImagePullPolicy: helpers.DefaultImagePullPolicy,
-		Env:             envVars(clusterDomain, nsToWatch),
-	}
-}
-
-// NewDeploymentForOperator will return a new deployment for a MinIO Operator
-func NewDeploymentForOperator(opts OperatorOptions) *appsv1.Deployment {
-	return &appsv1.Deployment{
-		TypeMeta: metav1.TypeMeta{
-			APIVersion: "apps/v1",
-			Kind:       "Deployment",
-		},
-		ObjectMeta: metav1.ObjectMeta{
-			Name:      helpers.DeploymentName,
-			Namespace: opts.Namespace,
-		},
-		Spec: appsv1.DeploymentSpec{
-			Replicas: &helpers.DeploymentReplicas,
-			Selector: &metav1.LabelSelector{
-				MatchLabels: operatorLabels(),
-			},
-			Template: corev1.PodTemplateSpec{
-				ObjectMeta: metav1.ObjectMeta{
-					Labels: operatorLabels(),
-				},
-				Spec: corev1.PodSpec{
-					ServiceAccountName: helpers.DefaultServiceAccount,
-					Containers:         []corev1.Container{container(opts.Image, opts.ClusterDomain, opts.NSToWatch)},
-					ImagePullSecrets:   []corev1.LocalObjectReference{{Name: opts.ImagePullSecret}},
-				},
-			},
-		},
-	}
 }
