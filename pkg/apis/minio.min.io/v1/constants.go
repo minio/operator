f7a5d549452a255907439d660b98b332dce035a8
Daniel Valdivia <hola@danielvaldivia.com>
2021-04-19 10:40:48 -0700

    Update Console Version to v0.6.8

--- pkg/apis/minio.min.io/v1/constants.go
+++ pkg/apis/minio.min.io/v1/constants.go
@@ -104,7 +104,7 @@ const DefaultZoneName = "zone-0"
 // Console Related Constants
 
 // DefaultConsoleImage specifies the latest Console Docker hub image
-const DefaultConsoleImage = "minio/console:v0.6.3"
+const DefaultConsoleImage = "minio/console:v0.6.8"
 
 // ConsoleTenantLabel is applied to the Console pods of a Tenant cluster
 const ConsoleTenantLabel = "v1.min.io/console"
