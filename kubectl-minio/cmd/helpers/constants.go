b1ec0b41d9e886aef4d35584725ddf440e80d908
Daniel Valdivia <hola@danielvaldivia.com>
2021-04-19 14:27:03 -0700

    Update Console Version to v0.6.8 (#602)

--- kubectl-minio/cmd/helpers/constants.go
+++ kubectl-minio/cmd/helpers/constants.go
@@ -80,7 +80,7 @@ const (
 	DefaultKESImage = "minio/kes:v0.13.4"
 
 	// DefaultConsoleImage is the default console image used while creating tenant
-	DefaultConsoleImage = "minio/console:v0.6.3"
+	DefaultConsoleImage = "minio/console:v0.6.8"
 
 	// DefaultOperatorServiceName is the default service name for operator
 	DefaultOperatorServiceName = "operator"
