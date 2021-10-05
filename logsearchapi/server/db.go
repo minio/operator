8b28cc1388b057f377a3b7e261745de2babb7bba
Aditya Manthramurthy <aditya.mmy@gmail.com>
2021-10-05 16:54:54 -0700

    logsearchapi: Fix background partition creation logic

--- logsearchapi/server/db.go
+++ logsearchapi/server/db.go
@@ -132,8 +132,8 @@ func (c *DBClient) checkPartitionTableExists(ctx context.Context, table string, 
 	return true, nil
 }
 
-func (c *DBClient) createTablePartition(ctx context.Context, table Table) error {
-	partTimeRange := newPartitionTimeRange(time.Now())
+func (c *DBClient) createTablePartition(ctx context.Context, table Table, givenTime time.Time) error {
+	partTimeRange := newPartitionTimeRange(givenTime)
 	_, err := c.ExecContext(ctx, table.getCreatePartitionStatement(partTimeRange))
 	return err
 }
@@ -149,7 +149,7 @@ func (c *DBClient) createTableAndPartition(ctx context.Context, table Table) err
 		return err
 	}
 
-	return c.createTablePartition(ctx, table)
+	return c.createTablePartition(ctx, table, time.Now())
 }
 
 func (c *DBClient) createTables(ctx context.Context) error {
