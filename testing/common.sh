3fb7a99805ad8e70797df0a3be92d27b6711fb81
Daniel Valdivia <18384552+dvaldivia@users.noreply.github.com>
2022-01-04 23:29:25 -0800

    Testing Tenants with KES Integration on PRs
    Signed-off-by: Daniel Valdivia <18384552+dvaldivia@users.noreply.github.com>

--- testing/common.sh
+++ testing/common.sh
@@ -21,3 +21,55 @@ die() {
 }
 
 try() { "$@" || die "cannot $*"; }
+
+function setup_kind() {
+    try kind create cluster --config "${SCRIPT_DIR}/kind-config.yaml"
+    echo "Kind is ready"
+    try kubectl get nodes
+}
+
+function install_operator() {
+    echo "Installing Current Operator"
+
+    # TODO: Compile the current branch and create an overlay to use that image version
+    try kubectl apply -k "${SCRIPT_DIR}/../resources"
+
+    echo "Waiting for k8s api"
+    sleep 10
+    echo "Waiting for Operator Pods to come online (2m timeout)"
+
+    try kubectl wait --namespace minio-operator \
+	--for=condition=ready pod \
+	--selector=name=minio-operator \
+	--timeout=120s
+}
+
+function destroy_kind() {
+    kind delete cluster
+}
+
+function check_tenant_status() {
+    # Check MinIO is accessible
+
+    waitdone=0
+    totalwait=0
+    while true; do
+	waitdone=$(kubectl -n $1 get pods -l v1.min.io/tenant=$2 --no-headers | wc -l)
+	if [ "$waitdone" -ne 0 ]; then
+	    echo "Found $waitdone pods"
+	    break
+	fi
+	sleep 5
+	totalwait=$((totalwait + 5))
+	if [ "$totalwait" -gt 305 ]; then
+	    echo "Unable to create tenant after 5 minutes, exiting."
+	    try false
+	fi
+    done
+
+    echo "Tenant is created successfully, proceeding to validate 'mc admin info minio/'"
+
+    kubectl run admin-mc -i --tty --image minio/mc --command -- bash -c "until (mc alias set minio/ https://minio.$1.svc.cluster.local $3 $4); do echo \"...waiting... for 5secs\" && sleep 5; done; mc admin info minio/;"
+
+    echo "Done."
+}
--- testing/deploy-tenant.sh
+++ testing/deploy-tenant.sh
@@ -20,23 +20,7 @@ export SCRIPT_DIR
 
 source "${SCRIPT_DIR}/common.sh"
 
-function setup_kind() {
-    try kind create cluster --config "${SCRIPT_DIR}/kind-config.yaml"
-}
 
-function install_operator() {
-    echo "Installing Current Operator"
-
-    # TODO: Compile the current branch and create an overlay to use that image version
-    try kubectl apply -k "${SCRIPT_DIR}/../resources"
-
-    echo "Waiting for Operator Pods to come online (2m timeout)"
-
-    try kubectl wait --namespace minio-operator \
-	--for=condition=ready pod \
-	--selector=name=minio-operator \
-	--timeout=120s
-}
 
 function install_tenant() {
     echo "Installing lite tenant"
@@ -70,36 +54,6 @@ function install_tenant() {
 }
 
 
-function check_tenant_status() {
-    # Check MinIO is accessible
-
-    waitdone=0
-    totalwait=0
-    while true; do
-	waitdone=$(kubectl -n tenant-lite get pods -l v1.min.io/tenant=storage-lite --no-headers | wc -l)
-	if [ "$waitdone" -ne 0 ]; then
-	    echo "Found $waitdone pods"
-	    break
-	fi
-	sleep 5
-	totalwait=$((totalwait + 5))
-	if [ "$totalwait" -gt 305 ]; then
-	    echo "Unable to create tenant after 5 minutes, exiting."
-	    try false
-	fi
-    done
-
-    echo "Tenant is created successfully, proceeding to validate 'mc admin info minio/'"
-
-    kubectl run admin-mc -i --tty --image minio/mc --command -- bash -c "until (mc alias set minio/ https://minio.tenant-lite.svc.cluster.local minio minio123); do echo \"...waiting... for 5secs\" && sleep 5; done; mc admin info minio/;"
-
-    echo "Done."
-}
-
-function destroy_kind() {
-    kind delete cluster
-}
-
 function main() {
     destroy_kind
 
@@ -109,7 +63,7 @@ function main() {
 
     install_tenant
 
-    check_tenant_status
+    check_tenant_status tenant-lite storage-lite minio minio123
 
     destroy_kind
 }
