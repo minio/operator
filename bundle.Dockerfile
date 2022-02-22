b8e6fa7f5d9809c1d72a04fb58bab08140cce3d5
Daniel Valdivia <18384552+dvaldivia@users.noreply.github.com>
2022-02-22 15:41:16 -0800

    OLM Files at repository root (#1027)
    Signed-off-by: Daniel Valdivia <18384552+dvaldivia@users.noreply.github.com>

--- bundle.Dockerfile
+++ bundle.Dockerfile
@@ -6,7 +6,6 @@ LABEL operators.operatorframework.io.bundle.manifests.v1=manifests/
 LABEL operators.operatorframework.io.bundle.metadata.v1=metadata/
 LABEL operators.operatorframework.io.bundle.package.v1=minio-operator
 LABEL operators.operatorframework.io.bundle.channels.v1=stable
-LABEL operators.operatorframework.io.metrics.builder=operator-sdk-v1.17.0
 
 
 # Copy files to locations specified by labels.
--- olm.sh
+++ olm.sh
@@ -18,3 +18,25 @@ myenv=$EXAMPLE yq -i e ".metadata.annotations.alm-examples |= (\"\${myenv}\" | e
 miniocontainer="quay.io/minio/operator:v$RELEASE" yq -i e '.metadata.annotations.containerImage |= env(miniocontainer)' bundles/$RELEASE/manifests/minio-operator.clusterserviceversion.yaml
 
 yq eval-all -i ". as \$item ireduce ({}; . * \$item )" bundles/$RELEASE/manifests/minio-operator.clusterserviceversion.yaml resources/templates/olm-template.yaml
+
+# Now promote the latest release to the root of the repository
+
+rm -Rf manifests
+rm -Rf metadata
+
+cp -R bundles/$RELEASE/manifests manifests
+cp -R bundles/$RELEASE/metadata metadata
+
+sed -i -e '/metrics/d' bundle.Dockerfile
+sed -i -e '/scorecard/d' bundle.Dockerfile
+sed -i -e '/testing/d' bundle.Dockerfile
+
+# clean released annotations
+sed -i -e '/metrics/d' bundles/$RELEASE/metadata/annotations.yaml
+sed -i -e '/scorecard/d' bundles/$RELEASE/metadata/annotations.yaml
+sed -i -e '/testing/d' bundles/$RELEASE/metadata/annotations.yaml
+
+# clean root level annotations.yaml
+sed -i -e '/metrics/d' metadata/annotations.yaml
+sed -i -e '/scorecard/d' metadata/annotations.yaml
+sed -i -e '/testing/d' metadata/annotations.yaml
