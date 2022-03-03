2ae189fc2d3bc9c75aa7203cb7f5f0c66b4c0d50
Daniel Valdivia <18384552+dvaldivia@users.noreply.github.com>
2022-03-03 15:04:35 -0800

    Release v4.4.11
    Signed-off-by: Daniel Valdivia <18384552+dvaldivia@users.noreply.github.com>

--- pkg/apis/minio.min.io/v2/constants.go
+++ pkg/apis/minio.min.io/v2/constants.go
@@ -85,7 +85,7 @@ const MinIOVolumeMountPath = "/export"
 const MinIOVolumeSubPath = ""
 
 // DefaultMinIOImage specifies the default MinIO Docker hub image
-const DefaultMinIOImage = "minio/minio:RELEASE.2022-02-24T22-12-01Z"
+const DefaultMinIOImage = "minio/minio:RELEASE.2022-03-03T21-21-16Z"
 
 // DefaultMinIOUpdateURL specifies the default MinIO URL where binaries are
 // pulled from during MinIO upgrades
@@ -147,7 +147,7 @@ const PrometheusHLSvcNameSuffix = "-prometheus-hl-svc"
 // Log related constants
 
 // DefaultLogSearchAPIImage specifies the latest logsearchapi container image
-const DefaultLogSearchAPIImage = "minio/operator:v4.4.10"
+const DefaultLogSearchAPIImage = "minio/operator:v4.4.11"
 
 // LogPgImage specifies the latest Postgres container image
 const LogPgImage = "library/postgres:13"
