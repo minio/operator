ab0df18a523b39149b83e08b431609c7ca36a8a4
Harshavardhana <harsha@minio.io>
2022-06-21 14:15:22 -0700

    Release v4.4.25
    Signed-off-by: Harshavardhana <harsha@minio.io>

--- kubectl-minio/cmd/tenant-upgrade.go
+++ kubectl-minio/cmd/tenant-upgrade.go
@@ -50,7 +50,7 @@ func newTenantUpgradeCmd(out io.Writer, errOut io.Writer) *cobra.Command {
 		Use:     "upgrade <TENANTNAME> --image <MINIO-IMAGE>",
 		Short:   "Upgrade MinIO image for existing tenant",
 		Long:    upgradeDesc,
-		Example: ` kubectl minio upgrade tenant1 --image quay.io/minio/minio:RELEASE.2022-06-17T02-00-35Z`,
+		Example: ` kubectl minio upgrade tenant1 --image quay.io/minio/minio:RELEASE.2022-06-20T23-13-45Z`,
 		Args: func(cmd *cobra.Command, args []string) error {
 			return c.validate(args)
 		},
--- pkg/apis/minio.min.io/v2/constants.go
+++ pkg/apis/minio.min.io/v2/constants.go
@@ -88,7 +88,7 @@ const MinIOVolumeMountPath = "/export"
 const MinIOVolumeSubPath = ""
 
 // DefaultMinIOImage specifies the default MinIO Docker hub image
-const DefaultMinIOImage = "minio/minio:RELEASE.2022-06-17T02-00-35Z"
+const DefaultMinIOImage = "minio/minio:RELEASE.2022-06-20T23-13-45Z"
 
 // DefaultMinIOUpdateURL specifies the default MinIO URL where binaries are
 // pulled from during MinIO upgrades
@@ -150,7 +150,7 @@ const PrometheusHLSvcNameSuffix = "-prometheus-hl-svc"
 // Log related constants
 
 // DefaultLogSearchAPIImage specifies the latest logsearchapi container image
-const DefaultLogSearchAPIImage = "minio/operator:v4.4.24"
+const DefaultLogSearchAPIImage = "minio/operator:v4.4.25"
 
 // LogPgImage specifies the latest Postgres container image
 const LogPgImage = "library/postgres:13"
