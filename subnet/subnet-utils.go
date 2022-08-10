7616c7d0b0aec74c7f51dcc1e4035870a7865889
Javier <reivaj0705@gmail.com>
2022-08-10 13:41:46 -0500

    Move subnet utils to common place

--- subnet/subnet-utils.go
+++ subnet/subnet-utils.go
@@ -13,7 +13,7 @@
 // You should have received a copy of the GNU Affero General Public License, version 3,
 // along with this program.  If not, see <http://www.gnu.org/licenses/>
 
-package cmd
+package subnet
 
 import (
 	"bufio"
