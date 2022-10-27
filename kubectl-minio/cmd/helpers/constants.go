291e6dfefd476b7774c7eb40f9f175e8c0a5e4e3
Daniel Valdivia <18384552+dvaldivia@users.noreply.github.com>
2022-10-27 12:04:15 -0700

    Release v4.5.4
    Signed-off-by: Daniel Valdivia <18384552+dvaldivia@users.noreply.github.com>

--- kubectl-minio/cmd/helpers/constants.go
+++ kubectl-minio/cmd/helpers/constants.go
@@ -68,10 +68,10 @@ const (
 	DefaultImagePullPolicy = corev1.PullIfNotPresent
 
 	// DefaultOperatorImage is the default operator image to be used
-	DefaultOperatorImage = "minio/operator:v4.5.3"
+	DefaultOperatorImage = "minio/operator:v4.5.4"
 
 	// DefaultTenantImage is the default MinIO image used while creating tenant
-	DefaultTenantImage = "minio/minio:RELEASE.2022-10-15T19-57-03Z"
+	DefaultTenantImage = "minio/minio:RELEASE.2022-10-24T18-35-07Z"
 
 	// DefaultKESImage is the default KES image used while creating tenant
 	DefaultKESImage = "minio/kes:v0.18.0"
