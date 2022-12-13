27e68c41cf91a6203169885ba7f7d11f39c7b7cc
pjuarezd <pjuarezd@users.noreply.github.com>
2022-12-13 16:36:24 -0600

    * add kubeconfig flag to proxy command * little refactor to simpify command parameters

--- kubectl-minio/cmd/proxy.go
+++ kubectl-minio/cmd/proxy.go
@@ -127,8 +127,13 @@ func servicePortForwardPort(ctx context.Context, namespace, service, port string
 		defer close(ch)
 		// service we are going to forward
 		serviceName := fmt.Sprintf("service/%s", service)
+		path, _ := rootCmd.Flags().GetString(kubeconfig)
+		parameters := []string{"port-forward", "--address", "0.0.0.0", "-n", namespace, serviceName, port}
+		if path != "" {
+			parameters = append([]string{"--kubeconfig", path}, parameters...)
+		}
 		// command to run
-		cmd := exec.CommandContext(ctx, "kubectl", "port-forward", "--address", "0.0.0.0", "-n", namespace, serviceName, port)
+		cmd := exec.CommandContext(ctx, "kubectl", parameters...)
 		// prepare to capture the output
 		var errStdout, errStderr error
 		stdoutIn, _ := cmd.StdoutPipe()
