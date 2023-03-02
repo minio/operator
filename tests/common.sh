d255ad65d18e203b501b03b1dedfc52d4ffdb5d0
Cesar Celis <celis.hernandez.cesar@gmail.com>
2023-03-02 10:26:02 -0500

    Using better path

--- tests/common.sh
+++ tests/common.sh
@@ -133,7 +133,7 @@ function install_tenant() {
 	value=storage-lite
 	echo "Installing lite tenant"
 
-	try kubectl apply -k "${SCRIPT_DIR}/../../../examples/kustomization/tenant-lite"
+	try kubectl apply -k "${GITHUB_WORKSPACE}/examples/kustomization/tenant-lite"
 
 	echo "Waiting for the tenant statefulset, this indicates the tenant is being fulfilled"
 	echo $namespace
