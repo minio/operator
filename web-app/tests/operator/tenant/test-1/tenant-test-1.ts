bf7d5eede878a71e04d71b14fa5c5c6cb16f2687
Daniel Valdivia <18384552+dvaldivia@users.noreply.github.com>
2023-03-02 10:41:59 -0800

    prettier write
    Signed-off-by: Daniel Valdivia <18384552+dvaldivia@users.noreply.github.com>

--- web-app/tests/operator/tenant/test-1/tenant-test-1.ts
+++ web-app/tests/operator/tenant/test-1/tenant-test-1.ts
@@ -14,90 +14,86 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import {Selector, t} from "testcafe";
+import { Selector, t } from "testcafe";
 import {
-    createTenant,
-    createTenantWithoutAuditLog,
-    deleteTenant,
-    goToPodInTenant,
-    goToPodSection,
-    goToPvcInTenant,
-    goToPvcSection,
-    loginToOperator,
+  createTenant,
+  createTenantWithoutAuditLog,
+  deleteTenant,
+  goToPodInTenant,
+  goToPodSection,
+  goToPvcInTenant,
+  goToPvcSection,
+  loginToOperator,
 } from "../../utils";
 
 fixture("For user with default permissions").page("http://localhost:9090");
 
 // Test 1
 test("Create Tenant and List Tenants", async (t) => {
-    const tenantName = `tenant-${Math.floor(Math.random() * 10000)}`;
-    await loginToOperator();
-    await createTenant(tenantName);
-    await deleteTenant(tenantName);
+  const tenantName = `tenant-${Math.floor(Math.random() * 10000)}`;
+  await loginToOperator();
+  await createTenant(tenantName);
+  await deleteTenant(tenantName);
 });
 
 // Test 2
 test("Create Tenant Without Audit Log", async (t) => {
-    const tenantName = `tenant-${Math.floor(Math.random() * 10000)}`;
-    await loginToOperator();
-    await createTenantWithoutAuditLog(tenantName);
-    await deleteTenant(tenantName);
+  const tenantName = `tenant-${Math.floor(Math.random() * 10000)}`;
+  await loginToOperator();
+  await createTenantWithoutAuditLog(tenantName);
+  await deleteTenant(tenantName);
 });
 
 // Test 3
 test("Test describe section for PODs in new tenant", async (t) => {
-    const tenantName = "storage-lite";
-    await loginToOperator();
-    await testPODDescribe(tenantName);
+  const tenantName = "storage-lite";
+  await loginToOperator();
+  await testPODDescribe(tenantName);
 });
 
 const testPODDescribe = async (tenantName: string) => {
-    await goToPodInTenant(tenantName);
-    await goToPodSection(1);
-    await checkPodDescribeHasSections();
+  await goToPodInTenant(tenantName);
+  await goToPodSection(1);
+  await checkPodDescribeHasSections();
 };
 
 const checkPodDescribeHasSections = async () => {
-    await t
-        .expect(Selector("#pod-describe-summary").exists)
-        .ok()
-        .expect(Selector("#pod-describe-annotations").exists)
-        .ok()
-        .expect(Selector("#pod-describe-labels").exists)
-        .ok()
-        .expect(Selector("#pod-describe-conditions").exists)
-        .ok()
-        .expect(Selector("#pod-describe-tolerations").exists)
-        .ok()
-        .expect(Selector("#pod-describe-volumes").exists)
-        .ok()
-        .expect(Selector("#pod-describe-containers").exists)
-        .ok();
+  await t
+    .expect(Selector("#pod-describe-summary").exists)
+    .ok()
+    .expect(Selector("#pod-describe-annotations").exists)
+    .ok()
+    .expect(Selector("#pod-describe-labels").exists)
+    .ok()
+    .expect(Selector("#pod-describe-conditions").exists)
+    .ok()
+    .expect(Selector("#pod-describe-tolerations").exists)
+    .ok()
+    .expect(Selector("#pod-describe-volumes").exists)
+    .ok()
+    .expect(Selector("#pod-describe-containers").exists)
+    .ok();
 };
 
 // Test 4
 test("Test describe section for PVCs in new tenant", async (t) => {
-    const tenantName = `storage-lite`;
-    await loginToOperator();
-    await testPvcDescribe(tenantName);
+  const tenantName = `storage-lite`;
+  await loginToOperator();
+  await testPvcDescribe(tenantName);
 });
 
 const testPvcDescribe = async (tenantName: string) => {
-    await goToPvcInTenant(tenantName);
-    await goToPvcSection(1);
-    await checkPvcDescribeHasSections();
+  await goToPvcInTenant(tenantName);
+  await goToPvcSection(1);
+  await checkPvcDescribeHasSections();
 };
 
 const checkPvcDescribeHasSections = async () => {
-    await t
-        .expect(Selector("#pvc-describe-summary").exists)
-        .ok()
-        .expect(Selector("#pvc-describe-annotations").exists)
-        .ok()
-        .expect(Selector("#pvc-describe-labels").exists)
-        .ok();
+  await t
+    .expect(Selector("#pvc-describe-summary").exists)
+    .ok()
+    .expect(Selector("#pvc-describe-annotations").exists)
+    .ok()
+    .expect(Selector("#pvc-describe-labels").exists)
+    .ok();
 };
-
-
-
-
