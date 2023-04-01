bd78f2c5da388f1efb72a6fa40edb6813d27253e
Harshavardhana <harsha@minio.io>
2023-03-31 17:01:24 -0700

    Release v5.0.2 (#1541)
    Signed-off-by: Harshavardhana <harsha@minio.io>

--- pkg/controller/operator.go
+++ pkg/controller/operator.go
@@ -47,7 +47,7 @@ const (
 	// DefaultDeploymentName is the default name of the operator deployment
 	DefaultDeploymentName = "minio-operator"
 	// DefaultOperatorImage is the version fo the operator being used
-	DefaultOperatorImage = "minio/operator:v5.0.1"
+	DefaultOperatorImage = "minio/operator:v5.0.2"
 )
 
 var serverCertsManager *xcerts.Manager
