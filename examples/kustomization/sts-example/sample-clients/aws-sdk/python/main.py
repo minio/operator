5bff281ced4aa59ec4c813cbe2ce6ea2c3544210
Daniel Valdivia <18384552+dvaldivia@users.noreply.github.com>
2023-06-05 21:21:46 -0700

    Updated python examples (#1621)
    Signed-off-by: Daniel Valdivia <18384552+dvaldivia@users.noreply.github.com>

--- examples/kustomization/sts-example/sample-clients/aws-sdk/python/main.py
+++ examples/kustomization/sts-example/sample-clients/aws-sdk/python/main.py
@@ -15,10 +15,11 @@
 # You should have received a copy of the GNU Affero General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-import boto3
 import os
 import sys
 from urllib.parse import urlparse
+
+import boto3
 
 sts_endpoint = os.getenv("STS_ENDPOINT")
 tenant_endpoint = os.getenv("MINIO_ENDPOINT")
@@ -29,7 +30,7 @@ policy_path = os.getenv("STS_POLICY")
 
 role_arn = "arn:aws:iam::111111111:dummyroot"
 role_session_name = "optional-session-name"
-os.environ.setdefault('AWS_ROLE_ARN', role_arn) #In AWS SDK RoleArn parameter is mandatory
+os.environ.setdefault('AWS_ROLE_ARN', role_arn)  # In AWS SDK RoleArn parameter is mandatory
 
 policy = None
 
@@ -40,13 +41,13 @@ if policy_path is not None:
 with open(token_path, "r") as f:
     sa_jwt = f.read()
 
-if sa_jwt == "" or sa_jwt == None:
+if sa_jwt == "" or sa_jwt is None:
     print("Token is empty")
     sys.exit(1)
 
-stsUrl = urlparse(f"{sts_endpoint}/{tenant_namespace}")
+sts_url = urlparse(f"{sts_endpoint}/{tenant_namespace}")
 
-sts = boto3.client('sts', endpoint_url=stsUrl.geturl(), verify=False)
+sts = boto3.client('sts', endpoint_url=sts_url.geturl(), verify=False)
 assumed_role_object = sts.assume_role_with_web_identity(
     RoleArn=role_arn,
     RoleSessionName=role_session_name,
@@ -58,12 +59,12 @@ assumed_role_object = sts.assume_role_with_web_identity(
 credentials = assumed_role_object['Credentials']
 print(credentials)
 
-tenantUrl = urlparse(tenant_endpoint)
+tenant_url = urlparse(tenant_endpoint)
 s3_client = boto3.resource('s3',
-    aws_access_key_id=credentials['AccessKeyId'],
-    aws_secret_access_key=credentials['SecretAccessKey'],
-    aws_session_token=credentials['SessionToken'],
-    endpoint_url=tenantUrl.geturl(), verify=False)
+                           aws_access_key_id=credentials['AccessKeyId'],
+                           aws_secret_access_key=credentials['SecretAccessKey'],
+                           aws_session_token=credentials['SessionToken'],
+                           endpoint_url=tenant_url.geturl(), verify=False)
 
 my_bucket = s3_client.Bucket(bucket)
 for my_bucket_object in my_bucket.objects.all():
