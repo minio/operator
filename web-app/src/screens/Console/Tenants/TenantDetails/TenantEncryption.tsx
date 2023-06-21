4176e04b258e1562ead19a5136e4f91f3d58391d
prakashsvmx <prakash@minio.io>
2023-06-21 16:59:35 +0530

    license header

--- web-app/src/screens/Console/Tenants/TenantDetails/TenantEncryption.tsx
+++ web-app/src/screens/Console/Tenants/TenantDetails/TenantEncryption.tsx
@@ -1,5 +1,5 @@
 // This file is part of MinIO Operator
-// Copyright (c) 2022 MinIO, Inc.
+// Copyright (c) 2023 MinIO, Inc.
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published by
