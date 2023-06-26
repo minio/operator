1a501f5396a4617434246a7441cb489d68f92999
guozhi.li <guozhi.li@daocloud.io>
2023-06-26 14:00:06 +0800

    words

--- pkg/controller/main-controller.go
+++ pkg/controller/main-controller.go
@@ -1124,7 +1124,7 @@ func (c *Controller) syncHandler(key string) error {
 				// Update failed, nothing needs to be changed in the container
 				return err
 			} else {
-				c.RegisterEvent(ctx, tenant, corev1.EventTypeWarning, "Set MINIO_UPDATE 'off', Shouldn't change image online!", fmt.Sprintf("Tenant %s", tenant.Name))
+				c.RegisterEvent(ctx, tenant, corev1.EventTypeWarning, "Set MINIO_UPDATE 'off', shouldn't change the image online!", fmt.Sprintf("Tenant %s", tenant.Name))
 			}
 		}
 		if err == nil {
