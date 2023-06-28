4a60456e11b17d88e2ed54b3865dba79e137ab70
Pedro Juarez <pjuarezd@users.noreply.github.com>
2023-06-27 18:21:14 -0600

    Release v5.0.6 (#1665)
    * Release v5.0.6
    
    ---------
    
    Signed-off-by: pjuarezd <pjuarezd@users.noreply.github.com>

--- docs/templates/asciidoctor/gv_list.tpl
+++ docs/templates/asciidoctor/gv_list.tpl
@@ -7,7 +7,7 @@
 [id="{p}-api-reference"]
 == API Reference
 
-:minio-image: https://hub.docker.com/r/minio/minio/tags[minio/minio:RELEASE.2023-05-27T05-56-19Z]
+:minio-image: https://hub.docker.com/r/minio/minio/tags[minio/minio:RELEASE.2023-06-23T20-26-00Z]
 :kes-image: https://hub.docker.com/r/minio/kes/tags[minio/kes:2023-05-02T22-48-10Z]
 
 {{ range $groupVersions }}
--- kubectl-minio/cmd/tenant-upgrade.go
+++ kubectl-minio/cmd/tenant-upgrade.go
@@ -49,7 +49,7 @@ func newTenantUpgradeCmd(out io.Writer, errOut io.Writer) *cobra.Command {
 		Use:     "upgrade <TENANTNAME> --image <MINIO-IMAGE>",
 		Short:   "Upgrade MinIO image for existing tenant",
 		Long:    upgradeDesc,
-		Example: ` kubectl minio upgrade tenant1 --image quay.io/minio/minio:RELEASE.2023-05-27T05-56-19Z`,
+		Example: ` kubectl minio upgrade tenant1 --image quay.io/minio/minio:RELEASE.2023-06-23T20-26-00Z`,
 		Args: func(cmd *cobra.Command, args []string) error {
 			return c.validate(args)
 		},
