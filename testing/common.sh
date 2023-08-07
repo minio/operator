64f6d813028755a7d7e3647e36a9db120a140f9d
Cesar Celis Hernandez <celis.hernandez.cesar@gmail.com>
2023-08-07 11:47:54 -0600

    Reusing testing code across repos (#1522)
    Reusing code across repos

--- testing/common.sh
+++ testing/common.sh
@@ -13,6 +13,8 @@
 # You should have received a copy of the GNU Affero General Public License, version 3,
 # along with this program.  If not, see <http://www.gnu.org/licenses/>
 
+source "${GITHUB_WORKSPACE}/shared-functions/shared-code.sh" # This is common.sh for k8s tests across multiple repos.
+
 ## this enables :dev tag for minio/operator container image.
 CI="true"
 export CI
@@ -179,58 +181,6 @@ function destroy_kind() {
   else
     kind delete cluster
   fi
-}
-
-function wait_for_resource() {
-  waitdone=0
-  totalwait=0
-  echo "command to wait on:"
-  command_to_wait="kubectl -n $1 get pods -l $3=$2 --no-headers"
-  echo $command_to_wait
-
-  while true; do
-    waitdone=$($command_to_wait | wc -l)
-    if [ "$waitdone" -ne 0 ]; then
-      echo "Found $waitdone pods"
-      break
-    fi
-    sleep 5
-    totalwait=$((totalwait + 5))
-    if [ "$totalwait" -gt 305 ]; then
-      echo "Unable to get resource after 5 minutes, exiting."
-      try false
-    fi
-  done
-}
-
-function wait_for_resource_field_selector() {
-  # example 1 job:
-  # namespace="minio-tenant-1"
-  # codition="condition=Complete"
-  # selector="metadata.name=setup-bucket"
-  # wait_for_resource_field_selector $namespace job $condition $selector
-  #
-  # example 2 tenant:
-  # wait_for_resource_field_selector $namespace job $condition $selector
-  # condition=jsonpath='{.status.currentState}'=Initialized
-  # selector="metadata.name=storage-policy-binding"
-  # wait_for_resource_field_selector $namespace tenant $condition $selector 900s
-
-  namespace=$1
-  resourcetype=$2
-  condition=$3
-  fieldselector=$4
-  if [ $# -ge 5 ]; then
-    timeout="$5"
-  else
-    timeout="600s"
-  fi
-
-  echo "Waiting for $resourcetype \"$fieldselector\" for \"$condition\" ($timeout timeout)"
-  kubectl wait -n "$namespace" "$resourcetype" \
-    --for=$condition \
-    --field-selector $fieldselector \
-    --timeout="$timeout"
 }
 
 function check_tenant_status() {
