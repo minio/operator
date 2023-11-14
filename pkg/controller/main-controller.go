e2b0094398681c11192763c8d4581734d5e5f534
jiuker <2818723467@qq.com>
2023-11-15 01:39:10 +0800

    fix: stopCh can't work for start operator (#1857)

--- pkg/controller/main-controller.go
+++ pkg/controller/main-controller.go
@@ -592,19 +592,22 @@ func (c *Controller) Start(threadiness int, stopCh <-chan struct{}) error {
 			Err:  nil,
 		}
 	}()
-
-	for oerr := range notificationChannel {
-		switch oerr.Type {
-		case STSServerNotification:
-			if !errors.Is(oerr.Err, http.ErrServerClosed) {
-				klog.Errorf("STS API Server stopped: %v, going to restart", oerr.Err)
-				go c.startSTSAPIServer(ctx, notificationChannel)
+	for {
+		select {
+		case oerr := <-notificationChannel:
+			switch oerr.Type {
+			case STSServerNotification:
+				if !errors.Is(oerr.Err, http.ErrServerClosed) {
+					klog.Errorf("STS API Server stopped: %v, going to restart", oerr.Err)
+					go c.startSTSAPIServer(ctx, notificationChannel)
+				}
+			case LeaderElection:
+				return nil
 			}
-		case LeaderElection:
+		case <-stopCh:
 			return nil
 		}
 	}
-	return nil
 }
 
 // Stop is called to shutdown the controller
