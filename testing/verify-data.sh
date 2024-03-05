6332d7bdb453363db5a1c400c5f06bd6b6cfc115
Cesar Celis Hernandez <celis.hernandez.cesar@gmail.com>
2024-03-05 17:57:25 -0500

    Make decommission test more reliable

--- testing/verify-data.sh
+++ testing/verify-data.sh
@@ -7,22 +7,24 @@ echo "Verify Data in remaining pool(s) after decommission test"
 echo "Get data and verify files are still present as they were uploaded"
 FILE_TO_SAVE_MC_OUTPUT=tmp.log
 rm -rf $FILE_TO_SAVE_MC_OUTPUT
-mc ls myminio/bucket >$FILE_TO_SAVE_MC_OUTPUT 2>&1
+mc ls myminio/bucket > $FILE_TO_SAVE_MC_OUTPUT 2>&1
 RESULT=$(grep -c ERROR $FILE_TO_SAVE_MC_OUTPUT | awk -F' ' '{print $1}')
 while [ "$RESULT" == "1" ]; do
 	echo "There was an error in mc ls, retrying"
-	mc ls myminio/bucket >$FILE_TO_SAVE_MC_OUTPUT 2>&1
+	mc ls myminio/bucket > $FILE_TO_SAVE_MC_OUTPUT 2>&1
 	RESULT=$(grep -c ERROR $FILE_TO_SAVE_MC_OUTPUT | awk -F' ' '{print $1}')
 	echo "${RESULT}"
 done
 echo "mc ls was successful"
+mc ls myminio/bucket # To see what is listed
 
 for i in {1..10}; do
 	RESULT=$(grep -c file"$i" "${FILE_TO_SAVE_MC_OUTPUT}")
 	if [ "${RESULT}" -eq 0 ]; then
 		echo "fail, there is a missing file: file${i}"
 		echo "exiting with 1 as there is missing file..."
+		echo "script failed" > verify-data.log
 		exit 1
 	fi
 done
-echo "script passed" >verify-data.log
+echo "script passed" > verify-data.log
