0589421ad3197a03fd935041a73abd10518612a7
Pedro Juarez <pjuarezd@users.noreply.github.com>
2024-05-12 22:24:43 -0700

    Update pkg/apis/sts.min.io/v1beta1/doc.go
    Co-authored-by: Shubhendu <shubhendu.tripathi@gmail.com>

--- pkg/apis/sts.min.io/v1beta1/doc.go
+++ pkg/apis/sts.min.io/v1beta1/doc.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2023, MinIO, Inc.
+// Copyright (C) 2024, MinIO, Inc.
 //
 // This code is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License, version 3,
