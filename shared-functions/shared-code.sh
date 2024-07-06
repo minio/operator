b70bfeec9be4e18ef76408db464faaf230b14cb2
Pedro Juarez <pjuarezd@users.noreply.github.com>
2024-07-06 09:31:01 -0600

    Add deploying a tenant with hotfix image test (#2202)
    Signed-off-by: pjuarezd <pjuarezd@users.noreply.github.com>

--- shared-functions/shared-code.sh
+++ shared-functions/shared-code.sh
@@ -65,3 +65,46 @@ function wait_for_resource_field_selector() {
     --field-selector $fieldselector \
     --timeout="$timeout"
 }
+
+# usage: wait_resource_status <namespace> <resourcetype> <resourcename> [<timeout>]
+function wait_resource_status() {
+	# This function will wait until the resource has a status field
+	# wait_resource_status ns-1 Tenant my-tenant
+	# wait_resource_status ns-1 Tenant my-tenant 1200
+
+	local namespace=$1
+	local resourcetype=$2
+	local resourcename=$3
+
+	# Timeout in seconds
+	if [ $# -ge 4 ]; then
+		TIMEOUT=$4
+	else
+		TIMEOUT=600
+	fi
+
+	START_TIME=$(date +%s)
+	# Interval in seconds between checks
+	INTERVAL=10
+
+	echo -e "${BLUE}Waiting for $resourcetype status to be created (${TIMEOUT}s timeout)${NC}"
+	while true; do
+		if kubectl get "$resourcetype" -n "$namespace" "$resourcename" -ojson | jq -e '.status' >/dev/null 2>&1; then
+			echo -e "${CHECK}$resourcetype/$resourcename status found."
+			break
+		else
+			echo "$resourcetype/$resourcename status not found, waiting for $INTERVAL seconds."
+			sleep $INTERVAL
+		fi
+
+		# Check timeout
+		CURRENT_TIME=$(date +%s)
+		# shellcheck disable=SC2004
+		ELAPSED_TIME=$(($CURRENT_TIME - $START_TIME))
+
+		if [ "$ELAPSED_TIME" -ge "$TIMEOUT" ]; then
+			echo "Timeout waiting for $resourcetype/$resourcename to be created."
+			exit 1
+		fi
+	done
+}
