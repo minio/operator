a13a9fec46fb026a1f20e5b7312eba2b86a38d5a
jiuker <jiuker@minio.io>
2024-07-26 16:57:32 +0800

    fields

--- pkg/apis/job.min.io/v1alpha1/types.go
+++ pkg/apis/job.min.io/v1alpha1/types.go
@@ -33,8 +33,8 @@ const (
 // +k8s:defaulter-gen=true
 // +kubebuilder:subresource:status
 // +kubebuilder:resource:scope=Namespaced,shortName=miniojob,singular=miniojob
-// +kubebuilder:printcolumn:name="Phase",type=string,JSONPath=`.spec.status.phase`
-// +kubebuilder:printcolumn:name="Message",type=string,JSONPath=`.spec.status.message`
+// +kubebuilder:printcolumn:name="Phase",type=string,JSONPath=`.status.phase`
+// +kubebuilder:printcolumn:name="Message",type=string,JSONPath=`.status.message`
 // +kubebuilder:metadata:annotations=operator.min.io/version=v6.0.1
 
 // MinIOJob is a top-level type. A client is created for it
